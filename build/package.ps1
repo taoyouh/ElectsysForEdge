fa2c4f6f55b7039a432b2b9a2f347a6e8578209c
Huang, Zhaoquan <zhaoquan2008@hotmail.com>
2017-04-09 19:00:48 +0800

    初次提交

---
+++ build/package.ps1
@@ -0,0 +1,6 @@
+﻿if ([String]::IsNullOrWhiteSpace($signtool))
+{
+    $signtool = "C:\Program Files (x86)\Windows Kits\10\bin\x86\signtool.exe"
+}
+manifoldjs -l debug -p edgeextension package .\edgeextension\manifest
+&$signtool sign /fd sha256 /a /f .\Electsys_StoreKey.pfx .\edgeextension\package\edgeExtension.appx
