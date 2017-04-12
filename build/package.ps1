9c7d93b8bacc84c6f19d2a6d3bf721b8ccc5111f
Huang, Zhaoquan <zhaoquan2008@hotmail.com>
2017-04-12 11:29:02 +0800

    添加AppVeyor自动化打包脚本

--- build/package.ps1
+++ build/package.ps1
@@ -1,6 +1,19 @@
-﻿if ([String]::IsNullOrWhiteSpace($signtool))
+﻿if ([String]::IsNullOrEmpty($env:APPVEYOR_BUILD_FOLDER))
+{
+    $rootPath = "./";
+}
+else
+{
+    $rootPath = $env:APPVEYOR_BUILD_FOLDER
+}
+
+$manifest = [System.IO.Path]::Combine($rootPath, "edgeextension\manifest")
+$key = [System.IO.Path]::Combine($rootPath, "Electsys_StoreKey.pfx")
+$appx = [System.IO.Path]::Combine($rootPath, "edgeextension\package\edgeExtension.appx")
+
+if ([String]::IsNullOrWhiteSpace($signtool))
 {
     $signtool = "C:\Program Files (x86)\Windows Kits\10\bin\x86\signtool.exe"
 }
-manifoldjs -l debug -p edgeextension package .\edgeextension\manifest
-&$signtool sign /fd sha256 /a /f .\Electsys_StoreKey.pfx .\edgeextension\package\edgeExtension.appx
+manifoldjs -l debug -p edgeextension package $manifest
+&$signtool sign /fd sha256 /a /f $key $appx
